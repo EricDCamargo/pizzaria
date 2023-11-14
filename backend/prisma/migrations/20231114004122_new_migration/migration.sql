95388ede0fc7c61dd8f12f366efd3faa7e7aaa5d
EricDCamargo <ericdcamargo@gmail.com>
2023-11-13 22:04:18 -0300

    feat: add and remove item from order

---
+++ backend/prisma/migrations/20231114004122_new_migration/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `items` table. All the data in the column will be lost.
+  - Added the required column `amount` to the `items` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "items" DROP COLUMN "name",
+ADD COLUMN     "amount" INTEGER NOT NULL,
+ADD COLUMN     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP;
