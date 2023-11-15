75c3abb5f4cde990b8ac2e7f9382e71fd9376334
EricDCamargo <ericdcamargo@gmail.com>
2023-11-14 23:17:21 -0300

    feat: setting up front-end

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça o login</title>
+      </Head>
+      <div>
+        <h1>Faça o seu login </h1>
+      </div>
+    </>
+  )
+}
