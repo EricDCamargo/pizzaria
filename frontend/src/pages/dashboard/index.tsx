502ceaa527f855e718d023373e37af525523de29
EricDCamargo <ericdcamargo@gmail.com>
2023-12-21 00:07:05 -0300

    feat: building dashboard layout

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,6 +1,9 @@
 import { canSSRAuth } from '@/src/utils/canSSRAuth'
 import Head from 'next/head'
 import { Header } from '@/src/components/Header'
+import styles from './styles.module.scss'
+import { FiRefreshCcw } from 'react-icons/fi'
+
 export default function DashBoard() {
   return (
     <>
@@ -9,7 +12,24 @@ export default function DashBoard() {
       </Head>
       <div>
         <Header />
-        <h1>Painel</h1>
+
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Últimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="3fffa3"></FiRefreshCcw>
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+            <section className={styles.orderItem}>
+              <button>
+                <div className={styles.tag}></div>
+                <span>Mesa 30</span>
+              </button>
+            </section>
+          </article>
+        </main>
       </div>
     </>
   )
