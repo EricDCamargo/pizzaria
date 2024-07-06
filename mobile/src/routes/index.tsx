c71dc68eab681c409223b467e68a985f2603b6d4
EricDCamargo <ericdcamargo@gmail.com>
2024-07-06 16:09:25 -0300

    loading effect

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,9 +1,26 @@
+import { ActivityIndicator, View } from 'react-native'
 import AppRoutes from './app.routes'
 import AuthRoutes from './auth.routes'
 
 function Routes() {
   const isAutenticated = false
   const loading = false
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#1D1D2E',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <ActivityIndicator size={60} color="#F5F7FB" />
+      </View>
+    )
+  }
+
   return isAutenticated ? <AppRoutes /> : <AuthRoutes />
 }
 export default Routes
